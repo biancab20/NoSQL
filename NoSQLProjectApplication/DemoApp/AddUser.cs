8743edb066fbeb616f139c4feb57d019af5c2067
br / <barboraazil@gmail.com>
2023-11-10 19:52:36 +0100

    user adding

---
+++ NoSQLProjectApplication/DemoApp/AddUser.cs
@@ -0,0 +1,50 @@
+﻿using Logic;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoApp
+{
+    public partial class AddUser : Form
+    {
+        private UserService userService;
+
+        public AddUser()
+        {
+            InitializeComponent();
+            userService = new UserService();
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string email = txtEmail.Text;
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            Role role = (Role)Enum.Parse(typeof(Role), cmbRole.Text);
+
+            User newUser = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Username = username,
+                Password = userService.Hash(password),
+                Role = role
+            };
+
+            userService.CreateUser(newUser);
+
+            MessageBox.Show("User added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
---
+++ NoSQLProjectApplication/DemoApp/UserManagement.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DemoApp
+{
+    partial class UserManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewUsers = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilterByEmail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewUsers
+            // 
+            this.listViewUsers.HideSelection = false;
+            this.listViewUsers.Location = new System.Drawing.Point(29, 194);
+            this.listViewUsers.Name = "listViewUsers";
+            this.listViewUsers.Size = new System.Drawing.Size(730, 244);
+            this.listViewUsers.TabIndex = 0;
+            this.listViewUsers.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Management";
+            // 
+            // txtFilterByEmail
+            // 
+            this.txtFilterByEmail.Location = new System.Drawing.Point(29, 131);
+            this.txtFilterByEmail.Name = "txtFilterByEmail";
+            this.txtFilterByEmail.Size = new System.Drawing.Size(156, 22);
+            this.txtFilterByEmail.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // UserManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFilterByEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewUsers);
+            this.Name = "UserManagement";
+            this.Text = "UserManagement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilterByEmail;
+        private System.Windows.Forms.Button button1;
+    }
+}
